e4d0bd53791953a58c0d90cab0e33211e1b1f70b
Ikaed <45915146+Ikaed@users.noreply.github.com>
2022-02-21 19:19:58 +0100

    Flyttningar.

--- Lab_2/Program.cs
+++ Lab_2/Program.cs
@@ -6,22 +6,31 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+  
+        var values = new List<string[]>();
+        var shapeCircle = new List<Circle>();
+        var shapeSquare = new List<Square>();
+        var hitSquare = new List<double>();
+        var missSquare = new List<double>();
+        var hitCircle = new List<double>();
+        var missCircle = new List<double>();
+
+        var inputDot = "";
+        var stringInput = "";
+        var startIndexHeader = 0;
+        var lengthIndexHeader = 23;
         var restartGame = false;
+
         do
         {
             restartGame = false;
-            var inputDot = "";
-            var stringInput = "";
+          
             Console.WriteLine("Mata in punkten (X,Y)");
             do
             {
                 inputDot = Console.ReadLine();
             } while (string.IsNullOrWhiteSpace(inputDot));
-
-
-            //shape ,X,Y,LENGTH ,POINTS;CIRCLE ,3,1,13,100; CIRCLE ,1,-1,15,200; square, -1 ,0 ,20 ,300; SQUARE , -3 ,2 ,8 ,400;
-
-
+            //X,shape, Y,POINTS, LENGTH; 3, CIRCLE, 1,100, 13; 1, CIRCLE, -1,200, 15; -1 ,square, 0 ,300, 20 ;  -3, SQUARE ,2 ,400, 8;
             Console.WriteLine("Mata in värdena");
 
             do
@@ -32,66 +41,33 @@ internal class Program
             try
             {
                 var trimmedInputDot = inputDot.Substring(1, inputDot.Length - 2);
-
                 var concatString = string.Concat(stringInput.Where(c => !char.IsWhiteSpace(c)));
-
-
-                var dotValue = Regex.Split(trimmedInputDot, @",");
-
-
-                var inputDotX = Convert.ToInt32(dotValue[0]);
-                var inputDotY = Convert.ToInt32(dotValue[1]);
-
-
-                // Header
-                var startIndexHeader = 0;
-                var lengthIndexHeader = 23;
-
                 var substringHeader = concatString.Substring(startIndexHeader, lengthIndexHeader);
-
-
+                var dotValue = Regex.Split(trimmedInputDot, @",");
                 var headerVariables = Regex.Split(substringHeader, @",|;");
-
                 var listVariables = Regex.Split(concatString, @";");
-
-
+                var inputDotX = Convert.ToInt32(dotValue[0]);
+                var inputDotY = Convert.ToInt32(dotValue[1]);           
+              
+                // Find index position for headers
                 var indexShape = Array.FindIndex(headerVariables, row => row.Contains("SHAPE"));
                 var indexX = Array.FindIndex(headerVariables, row => row.Contains("X"));
                 var indexY = Array.FindIndex(headerVariables, row => row.Contains("Y"));
                 var indexPoints = Array.FindIndex(headerVariables, row => row.Contains("POINTS"));
                 var indexLength = Array.FindIndex(headerVariables, row => row.Contains("LENGTH"));
-
-
-                var values = new List<string[]>();
-
-
+             
                 for (var i = 1; i <= listVariables.Length - 1; i++)
                 {
                     var seperateValues = Regex.Split(listVariables[i], @",");
                     values.Add(seperateValues);
                 }
-
+                foreach (var item in values)
+                {
+                    Console.WriteLine(item + "testc");
+                }
+              
                 // Remove last array
                 values.RemoveAt(values.Count - 1);
-
-                //var counterRow = 0;
-                //foreach (var item in values)
-                //{
-                //    var counterCol = 0;
-                //    Console.WriteLine("Row " + counterRow);
-                //    foreach (var i in item)
-                //    {
-                //        Console.WriteLine("Col " + counterCol + " " + i);
-                //        counterCol++;
-                //    }
-
-                //    counterRow++;
-                //}
-
-
-                var shapeCircle = new List<Circle>();
-                var shapeSquare = new List<Square>();
-
 
                 foreach (var row in values)
                 {
@@ -100,14 +76,10 @@ internal class Program
                     var length = Convert.ToInt32(row[indexLength]);
                     var points = Convert.ToInt32(row[indexPoints]);
 
-
                     if (row[indexShape] == "CIRCLE")
                         shapeCircle.Add(new Circle(x, y, length, points));
                     else if (row[indexShape] == "SQUARE") shapeSquare.Add(new Square(x, y, length, points));
                 }
-
-                var hitCircle = new List<double>();
-                var missCircle = new List<double>();
 
                 var dot = new Dot(inputDotX, inputDotY);
 
@@ -117,18 +89,13 @@ internal class Program
                     else
                         missCircle.Add(c.shapeScore());
 
-
-                var hitSquare = new List<double>();
-                var missSquare = new List<double>();
-
-
                 foreach (var s in shapeSquare)
                     if (s.Hit(dot.dotX, dot.dotY))
                         hitSquare.Add(s.shapeScore());
                     else
                         missSquare.Add(s.shapeScore());
 
-                // Presentation of the game result
+                // Output of the result
                 var game = new Game(missCircle, missSquare, hitSquare, hitCircle);
                 Console.WriteLine("Poängresultat: " + game.score);
             }
